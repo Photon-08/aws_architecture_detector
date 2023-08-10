5d96ad4fd5d73a651a90ccb1be686eb925c20a38
INDRANIL BHATTACHARYYA <125260412+Photon-08@users.noreply.github.com>
2023-08-10 13:46:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+import ultralytics
+from ultralytics import YOLO
+from PIL import Image
+import streamlit as st
+
+def engine(im):
+
+    model = YOLO("best (1).pt")
+    results = model(im)  # results list
+
+    # Show the results
+    for r in results:
+        im_array = r.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        im.show()  # show image
+        im.save('results.jpg')  # save image
+
+def main():
+    st.write("Testing")
+
+    from io import StringIO
+
+    user_im = st.file_uploader("Choose a file")
+    im = Image.open(user_im).convert('RGB').save('user_image.jpeg')
+    
+    
+    if user_im is not None:
+        st.image(user_im)
+        
+        
+
+    if st.button("Start the Prediction!"):
+        engine("user_image.jpeg")
+
+        with open("results.jpg", "rb") as file:
+            btn = st.download_button(
+                    label="Download image",
+                    data=file,
+                    file_name="prediction_image.jpg",
+                    mime="image/jpg"
+                )
+        
+    
+if __name__ == "__main__":
+    main()
